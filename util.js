041399d26e1e58f71f203aaa85e3eb1d92f14d7e
Yogendra Rampuria - Yogi <yogendrarampuria@gmail.com>
2023-02-15 00:32:09 +0800

    update to commonjs

--- util.js
+++ util.js
@@ -1,13 +1,16 @@
-import { inspect } from 'node:util'
-export async function sleep (seconds) {
+const inspect = require('util').inspect
+
+async function sleep (seconds) {
   return await new Promise((resolve, reject) => {
     setTimeout(resolve, seconds * 1000)
   })
 }
 
-export function debug (obj) {
+function debug (obj) {
   if (process.env.NODE_ENV !== 'development') {
     return
   }
   console.debug(inspect(obj, false, 5, true))
 }
+
+module.exports = { sleep, debug }
